90d691480e2c1aaccab49fe68ec16e77e996788f
golden-hamster <isack19999@gmail.com>
2024-03-20 22:17:25 +0900

    백준 2562 - 최댓값 구하기

---
+++ src/practice/pr240320.java
@@ -0,0 +1,29 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr240320 {
+    /**
+     * 백준 2562 - 최댓값
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[9];
+        int index = 0;
+        int max = 0;
+        for (int i = 0; i < 9; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+            if (max < a[i]) {
+                max = a[i];
+                index = i + 1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(index);
+
+    }
+}
